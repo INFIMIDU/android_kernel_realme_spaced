c716197b0849223f22841848dd501e73563e79a6
holyangel <slverwolf@gmail.com>
2016-06-12 12:03:26 -0500

    Kernel/sched: Reduce latency for better responsiveness
    Signed-off-by: HolyAngel <slverwolf@gmail.com>
    Signed-off-by: Salllz <sal235222727@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Anupam Roy <github.769@gmail.com>

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/sarthakroy2002/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
 git clone --depth=1 https://github.com/sarthakroy2002/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
 
-[ -d "out" ] && rm -rf out || mkdir -p out
+[ -d "out" ] && rm -rf AnyKernel && rm -rf out || mkdir -p out
 
 make O=out ARCH=arm64 spaced_defconfig
 
