fa3336b7e1cbdeb2409c1cad3dc549ff7b4ffb2e
INFIXHELL <mridulsharma57570@gmail.com>
2022-08-22 14:43:24 +0000

    kernel: Fix Section mismatches

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ make -j$(nproc --all) O=out \
 		              AR=llvm-ar \
 			          NM=llvm-nm \
 			          OBJCOPY=llvm-objcopy \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y
+                      CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 }
 
 function zupload()
